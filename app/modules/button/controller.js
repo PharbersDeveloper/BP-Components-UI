a1e34300e87db6f8582701f62655638a695367dc
Frank <wsgoodman@163.com>
2019-02-24 19:55:38 +0800

    button 展示完成。
    TODO: 展示代码所用的插件要更换。

--- app/modules/button/controller.js
+++ app/modules/button/controller.js
@@ -1,31 +1,41 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-	btnDefault: `{{#bp-button}}default{{/bp-button}}`,
-	btnPrimary: `{{#bp-button type="primary"}}Primary{{/bp-button}}`,
-	btnDisabled: `{{#bp-button disabled=true}}disabled button{{/bp-button}}`,
-	btnBlock: `{{#bp-button block=true onClick=(action 'submit')}}block button{{/bp-button}}`,
-	btnBasic: `{{#bp-button type="primary" onClick=(action 'submit')}}primary{{/bp-button}}
-		{{#bp-button type="secondary" onClick=(action 'submit')}}secondary{{/bp-button}}
-		{{#bp-button type="danger" onClick=(action 'submit')}}danger{{/bp-button}}
-		{{#bp-button type="default" disabled=false onClick=(action 'submit')}}default{{/bp-button}}
-		{{#bp-button type="default" disabled=true}}disabled default{{/bp-button}}
-		{{#bp-button type="primary"
-	            	onClick=(action 'submit')
-	            	icon="https://s3.amazonaws.com/uifaces/faces/twitter/damenleeturks/128.jpg"}}
-	        primary
-		{{/bp-button}}`,
-	btnReverse: `{{#bp-button type="primary" reverse=true onClick=(action 'submit')}}reverse primary{{/bp-button}}
-		{{#bp-button type="secondary" reverse=true onClick=(action 'submit')}}reverse secondary{{/bp-button}}
-		{{#bp-button type="danger" reverse=true onClick=(action 'submit')}}reverse danger{{/bp-button}}
-		{{#bp-button type="default" reverse=true disabled=true}}reverse default{{/bp-button}}
-		{{#bp-button type="primary" 
-	            	reverse=true 
-	            	onClick=(action 'submit')
-	            	icon="https://s3.amazonaws.com/uifaces/faces/twitter/damenleeturks/128.jpg"}}reverse
-		    primary
-		{{/bp-button}}`,
-
+	btnDefault: `
+	{{#bp-button}}default{{/bp-button}}`,
+	btnPrimary: `
+	{{#bp-button type="primary"}}Primary{{/bp-button}}`,
+	btnDisabled: `
+	{{#bp-button disabled=true}}disabled button{{/bp-button}}`,
+	btnBlock: `
+	{{#bp-button block=true onClick=(action 'submit')}}block button{{/bp-button}}`,
+	btnBasic: `
+	{{#bp-button type="primary" onClick=(action 'submit')}}primary{{/bp-button}}
+	{{#bp-button type="danger" onClick=(action 'submit')}}danger{{/bp-button}}
+	{{#bp-button type="warning" onClick=(action 'submit')}}Warning{{/bp-button}}
+	{{#bp-button type="link" onClick=(action 'submit')}}Link{{/bp-button}}
+	{{#bp-button type="link-subtle" onClick=(action 'submit')}}Link-Subtle{{/bp-button}}
+	{{#bp-button type="subtle" onClick=(action 'submit')}}Subtle{{/bp-button}}
+	{{#bp-button type="default" onClick=(action 'submit')}}default{{/bp-button}}`,
+	btnTotalDisabled: `
+	{{#bp-button type="primary" disabled=true onClick=(action 'submit')}}primary{{/bp-button}}
+	{{#bp-button type="danger" disabled=true onClick=(action 'submit')}}danger{{/bp-button}}
+	{{#bp-button type="warning" disabled=true onClick=(action 'submit')}}Warning{{/bp-button}}
+	{{#bp-button type="link" disabled=true onClick=(action 'submit')}}Link{{/bp-button}}
+	{{#bp-button type="link-subtle" disabled=true onClick=(action 'submit')}}Link-Subtle{{/bp-button}}
+	{{#bp-button type="subtle" disabled=true onClick=(action 'submit')}}Subtle{{/bp-button}}
+	{{#bp-button type="default" disabled=true}}disabled default{{/bp-button}}`,
+	btnIcon: `
+	{{#bp-button type="primary"
+		onClick=(action 'submit')
+		icon="https://s3.amazonaws.com/uifaces/faces/twitter/damenleeturks/128.jpg"}}
+		primary
+	{{/bp-button}}
+	{{#bp-button type="primary"
+		onClick=(action 'submit')
+		iconOnly=true
+		icon="https://s3.amazonaws.com/uifaces/faces/twitter/damenleeturks/128.jpg"}}
+	{{/bp-button}}`,
 	actions: {
 		submit() {
 			// window.alert('click btn');
