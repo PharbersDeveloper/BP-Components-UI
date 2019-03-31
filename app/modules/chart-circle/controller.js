4a6a18cd16211d02eaf8f698a2c97e6ad8dc5083
Frank <wsgoodman@163.com>
2019-03-31 17:01:48 +0800

    仪表盘 以及 icon 的展示。

--- app/modules/chart-circle/controller.js
+++ app/modules/chart-circle/controller.js
@@ -1,4 +1,12 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+
+	init() {
+		this._super(...arguments);
+		this.set('opts', {
+			width: '500px',
+			height: '500px'
+		})
+	}
 });
