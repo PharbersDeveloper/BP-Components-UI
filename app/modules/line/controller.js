8e94d8d7feeaac7fe44adde7a6a94309ef96a8be
Frank <wsgoodman@163.com>
2019-04-10 19:04:51 +0800

    tm 图表的样式展示。

--- app/modules/line/controller.js
+++ app/modules/line/controller.js
@@ -2,6 +2,27 @@ import Controller from '@ember/controller';
 import { A } from '@ember/array';
 
 export default Controller.extend({
+	lineDataTm: A([{
+		name: 'dataA',
+		date: ['2018年第一季度', '2018年第二季度', '2018年第三季度', '2018年第四季度'],
+		data: [320, 332, 301, 334]
+	},
+	{
+		name: 'DataB',
+		date: ['2018年第一季度', '2018年第二季度', '2018年第三季度', '2018年第四季度'],
+		data: [820, 932, 901, 934]
+	},
+	{
+		name: 'DataC',
+		date: ['2018年第一季度', '2018年第二季度', '2018年第三季度', '2018年第四季度'],
+		data: [420, 555, 509, 364]
+	},
+	{
+		name: 'DataD',
+		date: ['2018年第一季度', '2018年第二季度', '2018年第三季度', '2018年第四季度'],
+		data: [470, 439, 117, 769]
+	}]),
+	lineColorTm: A(['#57D9A3', '#79E2F2', '#FFE380', '#8777D9 ']),
 	init() {
 		this._super(...arguments);
 		this.set('lineData', A([{
@@ -19,5 +40,6 @@ export default Controller.extend({
 			data: [820, 932, 901, 934, 1290, 1330, 1320, 244, 365, 109, 203, 273]
 		}]));
 		this.set('lineColor', A(['#0070c0', '#c00000']));
+		this.set('legendPosition', { left: 'center' });
 	}
 });
