b56293d084364124179dba8ed5b609b31b462cab
Frank <wsgoodman@163.com>
2019-05-17 15:08:20 +0800

    增加 bp-bar-stack 的展示。

--- app/modules/charts/route.js
+++ app/modules/charts/route.js
@@ -11,7 +11,11 @@ export default Route.extend({
 				[[55555, 57, 100000, 'test3']],
 				[[33333, 45, 100000, 'test4']],
 				[[22222, 57, 255555, 'test5']]
-			])
+			]),
+			stackData: A([
+				{ name: 'keyong', data: [5, 20, 36, 10, 10, 20] },
+				{ name: 'bukeyong', data: [40, 22, 18, 35, 42, 40] },
+				{ name: 'qita', data: [40, 22, 18, 35, 42, 40] }])
 		})
 	}
 });
