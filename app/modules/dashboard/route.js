e124b9eee6fe859b2e28dcbafdf8a04d7a3b04df
frank <wangsen9108@gmail.com>
2019-07-22 18:55:43 +0800

    完成 pie / radar 的配置。

--- app/modules/dashboard/route.js
+++ app/modules/dashboard/route.js
@@ -95,6 +95,77 @@ export default Route.extend({
 					}
 				])
 			}]),
+			radar: {
+				height: 380,
+				panels: A([{
+					name: 'chart"s name',
+					id: 'radar1',
+					line: false,
+					bar: false,
+					pie: false,
+					radar: true,
+					stack: false,
+					scatter: false,
+					tooltip: {
+						show: true,
+						trigger: 'item',
+					},
+					radarConfig: {
+						radius: '65%'
+					},
+					legend: {
+						show: true,
+						x: 'center',
+						y: 'bottom',
+						// orient: 'vertical'
+					}
+				}])
+			},
+			circle: {
+				height: 300,
+				panels: A([
+					{
+						name: 'chart"s circle',
+						id: 'circle1',
+						line: false,
+						bar: false,
+						pie: true,
+						radar: false,
+						stack: false,
+						scatter: false,
+						tooltip: {
+							show: true,
+							trigger: 'item',
+						},
+						pieConfig: {
+							radius: '65%'
+						},
+						legend: {
+							show: true,
+							x: 'center',
+							y: 'bottom',
+							// orient: 'vertical'
+						},
+						pieConfigs: A([
+							{
+								insideRadius: '60%',
+								outsideRadius: '95%',
+								avoidLabelOverlap: false,
+								hoverOffset: 3, // 高亮扇区的偏移距离
+								label: {
+									normal: {
+										show: false,
+										position: 'center', // 'outside'/'inside'/'center'
+									},
+									emphasis: {
+										show: true
+									}
+								}
+							}
+						])
+					}
+				])
+			},
 			// time 不应该放入这里面，应该由某一个时间选择器选择的值
 			time: {
 				from: 1514736000000,
