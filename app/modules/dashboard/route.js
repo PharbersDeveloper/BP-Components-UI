1c1287743cf4547e1d75e53d8ceafe69009a8098
frank <wangsen9108@gmail.com>
2019-08-02 16:33:08 +0800

    其他图表 demo 的更新

--- app/modules/dashboard/route.js
+++ app/modules/dashboard/route.js
@@ -899,7 +899,7 @@ export default Route.extend({
 			},
 			tmProductCircle0Condition: [{
 				dynamic: {
-					isDynamic: true,
+					isDynamic: false,
 				},
 				data: {
 					"_source": [
@@ -1919,7 +1919,267 @@ export default Route.extend({
 					}
 				]
 			},
+			tmProdsLines: {
+				id: 'tmProdsLinesContainer',
+				height: 305,
+				panels: [{
+					id: 'demoline1',
+					xAxis: {
+						show: true,
+						type: 'category',
+						name: '',
+						axisTick: {
+							show: true,
+							alignWithLabel: true
+						},
+						axisLine: {
+							show: true,
+							lineStyle: {
+								type: 'dotted',
+								color: '#DFE1E6'
+							}
+						},
+						axisLabel: {
+							show: true,
+							color: '#7A869A',
+							fontSize: 14,
+							lineHeight: 20
+						}
+					},
+					yAxis: {
+						show: true,
+						type: 'value',
+						axisLine: {
+							show: false
+						},
+						axisTick: {
+							show: false
+						},
+						axisLabel: {
+							show: true,
+							color: '#7A869A',
+							// formatter: function (value) {
+							// 	return value * 100 + axisConfig.unit;
+							// }
+						},
+						splitLine: {
+							show: true,
+							lineStyle: {
+								type: 'dotted',
+								color: '#DFE1E6'
+							}
+						}
+					},
+					tooltip: {
+						show: true,
+						trigger: 'axis',
+						axisPointer: { // 坐标轴指示器，坐标轴触发有效
+							type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+						},
+						backgroundColor: 'rgba(9,30,66,0.54)'
+					},
+					legend: {
+						show: true,
+						x: 'center',
+						y: 'top',
+						orient: 'horizontal',
+						textStyle: {
+							fontSize: 14,
+							color: '#7A869A'
+						}
+					},
+					series: [{
+						type: 'line',
+						// 	seriesLayoutBy: 'row'
+					}, {
+						type: 'line',
+						// 	seriesLayoutBy: 'row'
+					}, {
+						type: 'line',
+						// 	seriesLayoutBy: 'row'
+					}, {
+						type: 'line',
+						// 	seriesLayoutBy: 'row'
+					}]
+				}]
+			},
+			tmProdsLinesCondition: [{
+				queryAddress: {
+					host: 'http://192.168.100.157',
+					port: 9000,
+					sheet: 'tmchart',
+					rule: 'pivot'
+				},
+				data: {
 
+					"_source": [
+						"date",
+						"product",
+						"salesRate"
+					],
+					"query": {
+						"bool": {
+							"must": [
+								{
+									"match": {
+										"rep": "all"
+									}
+								},
+								{
+									"match": {
+										"region": "all"
+									}
+								},
+								{
+									"match": {
+										"hosp_level": "all"
+									}
+								},
+								{
+									"match": {
+										"hosp_name": "all"
+									}
+								}
+							],
+							"must_not": [
+								{
+									"match": {
+										"product": "all"
+									}
+								},
+								{
+									"match": {
+										"date": "all"
+									}
+								}
+							]
+						}
+					},
+					"sort": [
+						{ "date": "asc" },
+						{ "product": "asc" }
+					]
+				}
+			}],
+			tmRadar: {
+				id: 'tmRadar01',
+				height: 356,
+				panels: [{
+					name: 'chart"s name',
+					id: 'tmRadaro1',
+					tooltip: {
+						show: true,
+						trigger: 'item',
+					},
+					color: ['#3172E0', '#979797'],
+					legend: {
+						show: true,
+						x: 'center',
+						y: 'bottom',
+						orient: 'vertical'
+					},
+					radar: {
+						radius: '65%',
+						name: {
+							textStyle: {
+								color: '#7A869A',
+								borderRadius: 3,
+								padding: [0, 0]
+							}
+						},
+						indicator: [
+							{ text: '产品知识', max: 1 },
+							{ text: '工作积极性', max: 1 },
+							{ text: '行为有效性', max: 1 },
+							{ text: '区域管理能力', max: 1 },
+							{ text: '销售知识', max: 1 }
+						],
+						splitNumber: 5, //default
+						axisLine: {
+							lineStyle: {
+								color: '#DFE1E6'
+							}
+						},
+						splitLine: {
+							lineStyle: {
+								color: '#DFE1E6'
+							}
+						},
+						splitArea: {
+							areaStyle: {
+								color: ['#fff', '#fff']
+							}
+						}
+					},
+					series: [{
+						name: '',
+						type: 'radar',
+
+						areaStyle: {
+							opacity: 0.3
+						},
+						encode: {
+							itemName: 0,
+							value: 0
+						}
+					}]
+
+				}]
+			},
+			tmRadarCondition: [{
+				data: {
+					"_source": [
+						"rep",
+						"product_knowledge",
+						"sales_skills",
+						"territory_management_ability",
+						"work_motivation",
+						"behavior_efficiency"
+					],
+					"query": {
+						"bool": {
+							"must": [
+								{
+									"match": {
+										"date": "2018Q1"
+									}
+								},
+								{
+									"match": {
+										"product": "all"
+									}
+								},
+								{
+									"match": {
+										"region": "all"
+									}
+								},
+								{
+									"match": {
+										"hosp_level": "all"
+									}
+								},
+								{
+									"match": {
+										"hosp_name": "all"
+									}
+								}
+							],
+							"must_not": [
+								{
+									"match": {
+										"rep": "all"
+									}
+								}
+							]
+						}
+					},
+					"sort": [
+						{ "rep": "asc" }
+					]
+				}
+			}
+			]
 		});
 	}
 });
