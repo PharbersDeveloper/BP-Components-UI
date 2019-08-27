0a5d1743101430bc41e38a58e75a476d2562379c
frank <wangsen9108@gmail.com>
2019-08-27 18:31:58 +0800

    bp-table 的展示以及新增 icon

--- app/modules/table/route.js
+++ app/modules/table/route.js
@@ -8,16 +8,15 @@ export default Route.extend({
                 label: '姓名',
                 valuePath: 'name',
                 align: 'left',
+                fixed: true,
                 // sortable: false,
                 width: '80'
-            },
-            {
+            }, {
                 label: htmlSafe('指标贡献率<br />2019Q1'),
                 valuePath: 'rate',
                 cellComponent: 'table/decimal-to-percentage',
                 align: 'center',
                 width: '100'
-
                 // sortable: false
             }, {
                 label: htmlSafe('YTD销售额<br />2019Q1'),
@@ -31,8 +30,7 @@ export default Route.extend({
                 valuePath: 'cc',
                 width: '120 '
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址d',
                 valuePath: 'dd',
                 width: '120'
@@ -42,68 +40,57 @@ export default Route.extend({
                 valuePath: 'ee',
                 width: '120'
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址这行f<br />第二行',
                 valuePath: 'ff',
                 width: '120 '
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址这行f<br />第二行',
                 valuePath: 'gg',
                 width: '120'
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址',
                 valuePath: 'hh',
                 width: '120 '
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址',
                 valuePath: 'ii',
                 width: '120 '
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址',
                 valuePath: 'jj',
                 width: '120 '
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址',
                 valuePath: 'kk',
                 width: '120 '
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址',
                 valuePath: 'll',
                 width: '120 '
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址',
                 valuePath: 'mm',
                 width: '120 '
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址',
                 valuePath: 'nn',
                 width: '120 '
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址',
                 valuePath: 'oo',
                 width: '120 '
                 // sortable: false
-            },
-            {
+            }, {
                 label: '地址',
                 valuePath: 'pp',
                 width: '120 '
@@ -200,44 +187,47 @@ export default Route.extend({
                 oo: "￥782,622",
                 pp: "￥706,33"
              },
-            // {
-            //     name: "宋楠5",
-            //     rate: 0.89,
-            //     ytd: 953697,
-            //     cc: "16.0%",
-            //     dd: "77.8%",
-            //     ee: 0,
-            //     ff: "-9.7%",
-            //     gg: "32.6%",
-            //     hh: "￥2,899,729",
-            //     ii: "￥843,948",
-            //     jj: "￥434,905",
-            //     kk: "￥859,432",
-            //     ll: "￥908,047",
-            //     mm: "￥688,335",
-            //     nn: "￥345,052",
-            //     oo: "￥782,622",
-            //     pp: "￥706,33"
-            // }, {
-            //     name: "宋楠6",
-            //     rate: 0.89,
-            //     ytd: 935975,
-            //     cc: "16.0%",
-            //     dd: "77.8%",
-            //     ee: 0,
-            //     ff: "-9.7%",
-            //     gg: "32.6%",
-            //     hh: "￥2,899,729",
-            //     ii: "￥843,948",
-            //     jj: "￥434,905",
-            //     kk: "￥859,432",
-            //     ll: "￥908,047",
-            //     mm: "￥688,335",
-            //     nn: "￥345,052",
-            //     oo: "￥782,622",
-            //     pp: "￥706,33"
-            // }
         ],
+        columnsPeople: [
+            {
+                label: '姓名',
+                valuePath: 'name',
+                width: '120 '
+                // sortable: false
+            }, {
+                label: '年纪',
+                valuePath: 'age',
+                width: '120 '
+                // sortable: false
+            }, {
+                label: '学校',
+                valuePath: 'school',
+                width: '120 '
+                // sortable: false
+            },{
+                label: '地址',
+                valuePath: 'address'
+                // sortable: false
+            },
+        ],
+        peopleData: [
+            {
+                name: 'brush lee',
+                age: 33,
+                school: 'hongkong middle school',
+                address: 'Macau SAR ChinaBbIOtrtock'
+            },{
+                name: 'brush wang',
+                age: 13,
+                school: 'hongkong middle school',
+                address: 'Fijisure pack as realize means quick development visit leaving tribe upper valuable machine attempt particularly pure is combination section soon volume sometime beat ought'
+            },{
+                name: 'brush zhang',
+                age: 23,
+                school: 'hongkong middle school',
+                address: 'Mongoliabend doubt master problem alike express yellow none practice honor married musical stop according town liquid on frighten map screen part action pure plates'
+            },
+        ]
         }
     }
 });
