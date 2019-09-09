4b59d0c7fa3d5549f84676f2fba11a2c100c0caa
frank <wangsen9108@gmail.com>
2019-09-09 11:42:34 +0800

    delete files only dist files

--- app/modules/empty-state-primary/controller.js
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-
-export default Controller.extend({
-	actions: {
-		primaryClick() {
-			window.alert('has click primary btn');
-		}
-	}
-});
--- app/modules/radio-checkbox/route.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-	model() {
-		return [{ id: 1, value: 'man', label: 'man-label' },
-		{ id: 2, value: 'women', label: 'women-babel' },
-		{ id: 3, value: 'unknow', label: 'unknow-babel' }];
-	}
-});
